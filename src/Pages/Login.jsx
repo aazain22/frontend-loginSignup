d61ca0e2ee0072d102d03292f4fb19fcececfdb1
aazain22 <76837223+aazain22@users.noreply.github.com>
2024-09-23 12:46:10 +0530

    ui change

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -10,7 +10,8 @@ export default function Login() {
     email: '',
     password: '',
   });
-
+const [err, setErr]= useState(false)
+const [isSend, setIsSend]= useState(false)
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
@@ -29,6 +30,8 @@ export default function Login() {
         body: JSON.stringify(loginInfo),
       });
       const result = await response.json();
+
+      setIsSend(true);
       const { email, success, message, name, jwt, error } = result;
       if (success) {
         handleSuccess(message);
@@ -41,6 +44,7 @@ export default function Login() {
         const details = error?.details[0].message;
         handleError(details);
       } else if (!success) {
+        setErr(message)
         handleError(message);
       }
       console.log(result);
@@ -111,6 +115,11 @@ export default function Login() {
               </Link>
             </p>
           </form>
+          <div>
+            {isSend ?  err? <p>{err}</p> :<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+</svg>: ''}
+          </div>
           {/* <ToastContainer /> */}
         </div>
       </div>
